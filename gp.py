c83b5f5b7d15f9eef6e8d3351591e3b232a377c4
Waylon Calabrese <hiwaylon@gmail.com>
2011-11-09 06:54:46 -0600

    comput marathon pace

--- gp.py
+++ gp.py
@@ -13,18 +13,18 @@ app = Flask(__name__)
 
 # TODO - Move to lib/ or some Flask config structure
 HUNDRED_METER_CONVERSION = 42195.0 / 100.0
+HUNDRED_TO_MARATHON = 16.09
 
 def _get_time_from_seconds(time_seconds):
     hours = int(time_seconds / 3600.0)
-    print(hours)
     time_seconds = time_seconds - hours * 3600.0
-    print(time_seconds)
     minutes = int(time_seconds / 60.0)
-    print(minutes)
     time_seconds = time_seconds - minutes * 60.0
     seconds = int(time_seconds)
-    print(seconds)
     return datetime.time(hours, minutes, seconds)
+
+def _compute_marathon_pace(hundred_pace_seconds):
+    return hundred_pace_seconds * HUNDRED_TO_MARATHON
 
 @app.route("/")
 def hello_world():
@@ -40,13 +40,16 @@ def paces():
     time = datetime.datetime.strptime(request.args["time"], "%H:%M:%S")
     time_seconds = (time.hour * 3600.0) + (time.minute * 60.0) + time.second
     hundred_pace_seconds = time_seconds / HUNDRED_METER_CONVERSION
+    marathon_pace_seconds = _compute_marathon_pace(hundred_pace_seconds)
    
     # NOTE - Do we care about rounding or microsecods?
     hundred_pace = _get_time_from_seconds(hundred_pace_seconds)
-    
+    marathon_pace = _get_time_from_seconds(marathon_pace_seconds)
+
     response = {
         "time": request.args["time"],
-        "hundred_pace": hundred_pace.strftime("%H:%M:%S"),
+        "hundred_pace": hundred_pace.strftime("%M:%S"),
+        "marathon_pace": marathon_pace.strftime("%M:%S"),
     }
     return json.dumps(response)
 
--- test_gp.py
+++ test_gp.py
@@ -24,7 +24,7 @@ class TestGoalPace(unittest.TestCase):
         time = gp._get_time_from_seconds(3600)
         self.assertEquals(datetime.time(1), time)
 
-    def test_respods_with_pace(self):
+    def test_respods_with_time(self):
         response = self.app.get("/api/v1/paces?time=2:52:37")
         response = json.loads(response.data)
         self.assertTrue("time" in response)
@@ -35,7 +35,7 @@ class TestGoalPace(unittest.TestCase):
         response = self.app.get("/api/v1/paces?time=3:00:00")
         response = json.loads(response.data)
         self.assertTrue("hundred_pace" in response)
-        self.assertEqual("00:00:25", response["hundred_pace"])
+        self.assertEqual("00:25", response["hundred_pace"])
 
         # The formula for computing 100m pace: goal_pace / (42195 / 100)
         pace = datetime.datetime.strptime("03:00:00", "%H:%M:%S")
@@ -43,6 +43,17 @@ class TestGoalPace(unittest.TestCase):
         hundred_pace = pace_seconds / gp.HUNDRED_METER_CONVERSION
         self.assertAlmostEqual(25.5954496978315, hundred_pace)
 
+    def test_response_with_marathon_pace(self):
+        """It should compute and respond with marathon pace."""
+        response = self.app.get("/api/v1/paces?time=2:52:37")
+        response = json.loads(response.data)
+        self.assertTrue("marathon_pace" in response, "Pace should be in response.")
+        self.assertEqual("06:34", response["marathon_pace"])
 
+    def test_compute_marathon_pace(self):
+        """It should correctly compute marathon pace from hundred pace."""
+        pace = gp._compute_marathon_pace(27.0)
+        self.assertEqual(434.43, pace)
+ 
 #if __name__ == "__main__":
 #        unittest.main()
